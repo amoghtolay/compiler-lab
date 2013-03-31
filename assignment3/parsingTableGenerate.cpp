de4acdd96ad87305b662834a652875702a19a5b9
amoghtolay <amoghtolay@gmail.com>
2013-04-01 00:14:40 +0530

    today's work

--- assignment3/parsingTableGenerate.cpp
+++ assignment3/parsingTableGenerate.cpp
@@ -24,16 +24,15 @@ using namespace std;
 /*
  * Prototype declarations
  */
-/*
- * For first and follow sets, use map of lists
- * This allows easier naming and finding
- */
 
 map < string,list < string > > getFirstSet ( char* firstFileName );
 map < string, list < string > > getFollowSet ( char* followFileName );
 void printTableToFile ( vector< vector<string> > table, char* outputFileName );
-list<int> getTerminals ( char*  prodRuleFileName );
-list<int> getNonTerminals ( char* prodRuleFileName );
+bool isInSet ( string tokenName, string setToken, map < string, list <string> > setName );
+map < string, map <string, string> > constructTable ( char* prodRuleFileName );
+list < string > getTermList ( char* prodRuleFileName );
+list < string > getNonTermList ( char* prodRuleFileName );
+vector < string > rhsTerms ( string productionRule );
 /*
  * Function to read first set from the file and store in RAM
  */
@@ -93,21 +92,117 @@ map < string,list < string > > getFollowSet ( char* followFileName ){
         return followSet;
 }
 
+bool isInSet ( string tokenName, string setToken, map < string, list <string> > setName ){
+	bool found = false;
+	for ( list < string >::iterator itList = setName[setToken].begin(); itList != setName[setToken].end(); ++itList ){
+		if ( *itList == tokenName ){
+			found = true;
+			break;
+		}
+	}
+	return found;
+}
+list < string > getNonTermList ( char* prodRuleFileName ){
+	list < string > nonTermList;
+	ifstream fpProdRule;
+	fpProdRule.open( prodRuleFileName );
+	if ( !fpProdRule.is_open() ){
+		perror("Production rule file could not be opened\n");
+		exit (1);
+        }
+	string line;
+	getline ( fpProdRule, line );
+	getline ( fpProdRule, line );
+	istringstream iss( line );
+	while (iss){
+		string nonTerm;
+		iss >> nonTerm;
+		nonTermList.push_back ( nonTerm );
+	}
+	return nonTermList;
+}
 
+list < string > getTermList ( char* prodRuleFileName ){
+	list < string > termList;
+	ifstream fpProdRule;
+	fpProdRule.open( prodRuleFileName );
+	if ( !fpProdRule.is_open() ){
+		perror("Production rule file could not be opened\n");
+		exit (1);
+        }
+	string line;
+	getline ( fpProdRule, line );
+	istringstream iss( line );
+	while (iss){
+		string term;
+		iss >> term;
+		termList.push_back ( term );
+	}
+	return termList;
+}
+map < string, map < string,string > > constructTable ( char* prodRuleFileName ){
+	map < string, map < string,string > > table;
+	list < string > termList = getTermList ( prodRuleFileName );
+	list < string > nonTermList = getNonTermList( prodRuleFileName );
+	// nonTerminals are in list, terminals are in list
+	// Now read production rule A->alpha, and then for each alpha, check if in first or follow of RHS
+	ifstream fpProdRule;
+	fpProdRule.open( prodRuleFileName );
+	if ( !fpProdRule.is_open() ){
+		perror("Production rule file could not be opened\n");
+		exit (1);
+        }
+	string prodRule;
+	// Ignoring the first two lines which are list of terms and nonterms
+	getline ( fpProdRule, prodRule );
+	getline ( fpProdRule, prodRule );
+	// Now populating a list of LHS and RHS components for each rule
+	while( getline ( fpProdRule, prodRule ) ){
+		list < string > oneRuleTerms;
+		unsigned pos = prodRule.find( "-->" );
+		if( pos != string::npos ){
+			string lhs = prodRule.substr( 0,pos );
+			oneRuleTerms.push_back( lhs );
+			prodRule = prodRule.substr( pos+3 );
+			istringstream iss( prodRule );
+			while( iss ){
+				string rhsTerm;
+				iss >> rhsTerm;
+				oneRuleTerms.push_back( rhsTerm );
+			}
+			oneRuleTerms.pop_back();
+		}
+		else{
+			perror("The production rule is without an arrow sign\n");
+			exit(1);
+		}
+		// Now for each rule, check the first and follow sets, and then appropriately add to the table
+
+	}
+	return table;
+}
 int main( int argc, char *argv[] ){
 	if ( argc < 5 ) {
 		cout<<"ERROR: No production rule file provided or no first set file or follow set file or output file provided.\n";
 		cout<<"Usage: ./tableGen prodRule firstFile followFile outputTableFile\n";
 		exit ( EXIT_FAILURE );
 	}
+	/*
+	 * Just testing code below, printing and stuff. Should be commented later on.
+	 */
+	/*
 	map < string, list < string > > firstSet = getFirstSet ( argv[2] );
 	map < string, list < string > > followSet = getFollowSet ( argv[3] );
-	
+	map < string, map < string, string > > table = constructTable ( argv[1] );	
 	for ( map < string, list <string> >::iterator itMap = firstSet.begin(); itMap != firstSet.end(); ++itMap ){
 		cout << itMap->first << " => \n";
 		for ( list < string >::iterator itList = firstSet[itMap->first].begin(); itList != firstSet[itMap->first].end(); ++itList )
 			cout << ' ' << *itList;
 		cout<<"\n";
 	}
+	list < string > termList = getTermList ( argv[1] );
+	for ( list < string >::iterator it=termList.begin(); it != termList.end(); ++it )
+		cout<<*it<<"\t";
+	*/
         return ( EXIT_SUCCESS );
 }
